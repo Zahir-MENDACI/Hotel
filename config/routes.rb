616c7b62053a4a21f3466de5ffe1c67015b6ecf5
Zahir <menzahir@gmail.com>
2021-01-29 10:48:36 +0100

    Final Version

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+
+  
+
+  root :to => 'pages#home'
+  get 'index' => "reservations#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :chambres do
+    resources :reviews
+  end
+  resources :reservations
+  resources :pages
+
+  # require "sidekiq/web"
+  # mount Sidekiq::Web => 'sidekiq'
+
+end
